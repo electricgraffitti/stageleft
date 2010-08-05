cdbac8697426d1aa71647915ee26d7b8acd74000
Larry Finn <Cube2@Larry-Finns-iMac.local>
2010-08-04 20:18:56 -0700

    setup image divs on redesign and staging pages in content div deleted an old php mail script

--- views/content/redesign_content.php
+++ views/content/redesign_content.php
@@ -1,6 +1,21 @@
 <div id="content"> 
 	<h2>Redesigning</h2>
 	<p>
-		Lobortis urna. Ut bibendum gravida metus, et vulputate nibh hendrerit at! Phasellus a metus lectus! Fusce id enim nisl, id suscipit sem. Praesent lobortis cursus arcu nec blandit? Integer ut risus nulla, sit amet cursus eros. Donec id nibh a felis tempor condimentum sed nec neque. Mauris bibendum; mi in sagittis fermentum, turpis felis rhoncus massa, id accumsan risus orci ut orci. Maecenas mollis leo ultricies dolor cursus lacinia at non dolor. Vivamus eu accumsan arcu. Praesent purus orci, congue eget iaculis at, placerat at tortor. Quisque erat nisl, posuere a ultricies vitae, vulputate a dui.
+		View some of our before and after images of our redesigning projects.
 	</p>
+	<div id="left">
+		<h2>Before</h2>
+		<img src="public/stylesheets/images/before1.jpg">
+		<img src="public/stylesheets/images/before1.jpg">
+		<img src="public/stylesheets/images/before1.jpg">
+		<img src="public/stylesheets/images/before1.jpg">
+		
+	</div>
+	<div id="right">
+		<h2>After</h2>
+		<img src="public/stylesheets/images/after1.jpg">
+		<img src="public/stylesheets/images/after1.jpg">
+		<img src="public/stylesheets/images/after1.jpg">
+		<img src="public/stylesheets/images/after1.jpg">
+	</div>
 </div><!--end content-->
