79132449e4f455dbe6a033992f3f344b358c4e0a
Larry Finn <Cube2@Larry-Finns-iMac.local>
2010-08-06 09:19:18 -0700

    fixed typography on testimonials added border for IE on p tags joe say bite me

--- views/content/testimonial_content.php
+++ views/content/testimonial_content.php
@@ -1,59 +1,55 @@
 <div id="content">
 	<h2>What Our Clients Have To Say</h2>
 	 	<p class="testimonial">
-	 		Dear Cristina,<br />
-	I know I am not easy to get along with, and I thank you for being so gracious.
-	Thank you for helping with the staging of our house.  You made it so lovely to show.  I could not believe the praising of the clients.  I wish I had called you earlier, maybe the house would have sold sooner.<br />
+	&#34;I know I am not easy to get along with&#44; and I thank you for being so gracious.
+	Thank you for helping with the staging of our house.  You made it so lovely to show.  I could not believe the praising of the clients.  I wish I had called you earlier&#44; maybe the house would have sold sooner.<br />
 	<br />
-	Thank you again,<br />
-	Sincerely yours,<br />
-	<span class="italic">Ron Rooter</span>
-	<span class="italic"><a href="http://www.stageleft.cinneman.com/public/testimonials/testimonial5.pdf">View Letter</a></span>
+	Thank you again&#44;<br />
+	Sincerely yours&#44;<br />
+	Ron Rooter
+	<a href="http://www.stageleft.cinneman.com/public/testimonials/testimonial5.pdf">View Letter</a>
 	 	</p>
 		<p class="testimonial">
-			To: Cristina LeBreton,<br />
-	"A Huge Thank You!" I sincerely appreciate all you have done to help my clients, but helping me also, my business has so much improved and my clients are thrilled.  We will be doing a lot of business together.  I will spread the word to my Realtor friends, especially in Henderson and Las Vegas.<br />
+	&#34;A Huge Thank You! I sincerely appreciate all you have done to help my clients&#44; but helping me also, my business has so much improved and my clients are thrilled.  We will be doing a lot of business together.  I will spread the word to my Realtor friends&#44; especially in Henderson and Las Vegas.&#34;<br />
 	<br />
-	As Always,<br />
-	<span class="italic">Pat Bernstein</span>
-		<span class="italic"><a href="http://www.stageleft.cinneman.com/public/testimonials/testimonial4.pdf">View Letter</a></span>		
+	As Always&#44;<br />
+	Pat Bernstein
+	<a href="http://www.stageleft.cinneman.com/public/testimonials/testimonial4.pdf">View Letter</a>	
 		</p>
 		<p class="testimonial">
-			Dear Cristina,<br />
-			Thank you so much for the beautiful job you did on 641 Mt. Bona.  My neighbors did not even recognize the place!  You made the flow of the home so comfortable, the art work showed so much better, you have a gift.  I will recommend you to my friends and neighbors, whether they are selling or just need a little "help" with decor.<br />
-			P.S.  The Priced was right too!!!<br />
+			&#34;Thank you so much for the beautiful job you did on 641 Mt. Bona.  My neighbors did not even recognize the place!  You made the flow of the home so comfortable&#44; the art work showed so much better&#44; you have a gift.  I will recommend you to my friends and neighbors&#44; whether they are selling or just need a little &#34;help&#34; with decor.&#34;  &#34;P.S. The Priced was right too!!!&#34;<br />
 		<br />
-		Sincerely,<br />
-		<span class="italic">Patty Worden</span><br />
-		<span class="italic">Lake Mountain Estates</span>
-		<span class="italic"><a href="http://www.stageleft.cinneman.com/public/testimonials/testimonial1.pdf">View Letter</a></span>
+		Sincerely&#44;<br />
+		Patty Worden<br />
+		Lake Mountain Estates
+		<a href="http://www.stageleft.cinneman.com/public/testimonials/testimonial1.pdf">View Letter</a>
 		</p>
 		<p class="testimonial">
-			Dear Cristina,<br />
-			I want to thank you so much for the fabulous job you did on Mt. Bona.  Your staging was so beautiful, neighbors and Realtors could not believe it was the same house they saw about a month ago.  You have a gift and it must be shared.  You made this very cluttered home into a most welcoming home without tripping all over "stuff" which shows the true beauty of what a home should be.<br />
-			Sincerely,<br />
-			<span class="italic">Pat Bernstein</span><br />
-			<span class="italic">Broker/Owner Lake Mead Area Realty</span><br />
-			<span class="italic"><a href="http://www.stageleft.cinneman.com/public/testimonials/testimonial2.pdf">View Letter</a></span>
+			&#34;I want to thank you so much for the fabulous job you did on Mt. Bona.  Your staging was so beautiful&#44; neighbors and Realtors could not believe it was the same house they saw about a month ago.  You have a gift and it must be shared.  You made this very cluttered home into a most welcoming home without tripping all over &#34;stuff&#34; which shows the true beauty of what a home should be.&#34;<br />
+			<br />
+			Sincerely&#44;<br />
+			Pat Bernstein<br />
+			Broker&#47;Owner Lake Mead Area Realty<br />
+			<a href="http://www.stageleft.cinneman.com/public/testimonials/testimonial2.pdf">View Letter</a>
 		</p>
 
 		<p class="testimonial">
-			Dear Cristina,<br />
-			Thank you so much for your help and the gift given you to stage a home.  The staging helped so very much with showing the homes with light and color and comfort.  I really appreciate what you have done - on all three properties!!!  My clients are also very pleased and had no idea what an improvement to their property your staging would do to help promote their homes.<br />
+			&#34;Thank you so much for your help and the gift given you to stage a home.  The staging helped so very much with showing the homes with light and color and comfort.  I really appreciate what you have done - on all three properties!!!  My clients are also very pleased and had no idea what an improvement to their property your staging would do to help promote their homes.&#34;<br />
+			<br />
 			Thank you again and hopefully we will do a lot of business together.<br />
-			Sincerely, <br />
-			<span class="italic">April Cummins</span><br />
-			<span class="italic">Broker/Manager Lake Mead Area Realty</span><br />
-			<span class="italic"><a href="http://www.stageleft.cinneman.com/public/testimonials/testimonial3.pdf">View Letter</a></span>
+			Sincerely&#44; <br />
+			April Cummins<br />
+			Broker&#47;Manager Lake Mead Area Realty<br />
+			<a href="http://www.stageleft.cinneman.com/public/testimonials/testimonial3.pdf">View Letter</a>
 		</p>
 
 		<p class="testimonial">
-			Dear Cristina,<br />
-			A million thanks.  My family loved the staging as well as my husband and I.  The baby gift is ever so appreciated.
-			Thank you again<br />
-			<span class="italic">Stacy and Bob Campbell</span><br />
-			<span class="italic">(and little Jake!)</span>
-			<span class="italic"><a href="http://www.stageleft.cinneman.com/public/testimonials/testimonial6.pdf">View Letter</a></span>
+			&#34;A Million thanks.  My family loved the staging as well as my husband and I.  The baby gift is ever so appreciated.&#34;<br />
+			<br />
+			Thank you again&#44;<br />
+			Stacy and Bob Campbell<br />
+			&#40;and little Jake!&#41;
+			<a href="http://www.stageleft.cinneman.com/public/testimonials/testimonial6.pdf">View Letter</a>
 		</p>
 
 </div><!--end content-->
